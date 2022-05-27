cac7d5f5067cb80a8e723959178a0bcfc7e1721b
Juan <pablogomez.nov@gmail.com>
2022-05-27 15:09:13 -0300

    cambio de prueba en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <h1 
           className="font-black text-5xl text-center md:w-2/3 mx-auto text-slate-200"
       >
-          Seguimiento de pacientes
+          Seguimiento de pacientes con GitHub
           <span className="text-emerald-500"> Veterinaria</span>
       </h1>
     </>
